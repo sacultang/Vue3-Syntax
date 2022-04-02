a285f4c5e807393f128b657a8612722f335f4cb1
sacultang <sacultang@gmail.com>
2022-04-02 12:12:05 +0900

    Syntax

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1 
+      :class="{ active : isActive }"
+      @click="activate()">Hello?!({{ isActive }})</h1>
+  </div>
+</template>
+
+<script>
+  // import Fruits from './components/Fruits'
+  export default {
+    components :{
+      // Fruits
+    },
+    data(){
+      return {
+        // Getter, Setter
+        msg: 'Hello Computed',
+        isActive: false
+      }
+    },
+    computed: {
+      // Getter
+      // revesedMessage(){
+      //   return this.msg.split('').reverse().join('')
+      // }
+      revesedMessage () {
+        return this.msg.split('').reverse().join('')
+      }
+    },
+    watch:{
+      msg(newValue){
+        console.log('msg watch: ',newValue)
+      },
+      revesedMessage(){
+        console.log(this.revesedMessage)
+      }
+    },
+    methods: {
+      add(){
+        this.msg = 'Good!'
+      },
+      activate() {
+        this.isActive = !this.isActive
+      }
+    }
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+  .active {
+    color: red;
+    font-weight: bold;
+  }
+</style>
---
+++ src/components/Fruits.vue
@@ -0,0 +1,43 @@
+<template>
+  <section v-if="hasFruit">
+    <h1>Fruits</h1>
+    <ul>
+      <li v-for="fruit in fruits" :key="fruit">
+        {{fruit}}
+      </li>
+    </ul>
+    <h1>Reverse Fruits</h1>
+    <ul>
+      <li v-for="fruit in reverseFruits" :key="fruit">
+        {{fruit}}
+      </li>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      fruits : [ 
+        'Apple','Banan','Cherry'
+      ]
+    }
+  },
+  computed:{
+    hasFruit(){
+      return this.fruits.length > 0
+    },
+    reverseFruits(){
+      return this.fruits.map(fruit => {
+        // split() 'Apple' => ['A','p','p','l','e']
+        // reverse() => ['e','l','p','p','A']
+        // join() => 'elppA'
+        return fruit.split('').reverse().join('')
+      })
+    }
+  }
+}
+</script>
+<style>
+  
+</style>
